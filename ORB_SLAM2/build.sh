455b7d61ac4a2c052811ca44c601eb282991a81f
PierBanda <37575086+PierBanda@users.noreply.github.com>
2018-06-11 16:05:52 +0200

    Update build.sh

--- ORB_SLAM2/build.sh
+++ ORB_SLAM2/build.sh
@@ -6,7 +6,7 @@ cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make
 
 cd ../../g2o
 
@@ -15,7 +15,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make
 
 cd ../../../
 
@@ -30,4 +30,4 @@ echo "Configuring and building ORB_SLAM2 ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make
